89b80510f08dcb7934925742e8cc7f578051534f
Winter.Vfz <lisemdarenne@gmail.com>
2026-05-18 20:43:08 +0100

    Create gpp.js

---
+++ commands/gpp.js
@@ -0,0 +1,63 @@
+// commands/gpp.js
+
+export const name = 'gpp'
+
+export async function execute({
+  sock,
+  m
+}) {
+
+  try {
+
+    if (
+      !m.key.remoteJid.endsWith('@g.us')
+    ) {
+
+      return
+    }
+
+    const quoted =
+      m.message?.extendedTextMessage
+      ?.contextInfo
+      ?.quotedMessage
+
+    if (!quoted?.imageMessage) {
+
+      return await sock.sendMessage(
+        m.key.remoteJid,
+        {
+          text:
+`╭━━〔 TESSIA GPP 〕━━⬣
+┃ ⚠️ Répond à une image
+╰━━━━━━━━━━━━⬣`
+        },
+        { quoted: m }
+      )
+    }
+
+    const media =
+      await sock.downloadMediaMessage({
+        message: quoted
+      })
+
+    await sock.updateProfilePicture(
+      m.key.remoteJid,
+      media
+    )
+
+    await sock.sendMessage(
+      m.key.remoteJid,
+      {
+        text:
+`╭━━〔 TESSIA GPP 〕━━⬣
+┃ ✅ Photo du groupe modifiée
+╰━━━━━━━━━━━━⬣`
+      },
+      { quoted: m }
+    )
+
+  } catch (e) {
+
+    console.log(e)
+  }
+}
