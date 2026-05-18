4d1271978fc26cc72815fbee43e4daf4c6dc9ced
Winter.Vfz <lisemdarenne@gmail.com>
2026-05-18 20:42:34 +0100

    Create gdesc.js

---
+++ commands/gdesc.js
@@ -0,0 +1,59 @@
+// commands/gdesc.js
+
+export const name = 'gdesc'
+
+export async function execute({
+  sock,
+  m,
+  args
+}) {
+
+  try {
+
+    if (
+      !m.key.remoteJid.endsWith('@g.us')
+    ) {
+
+      return
+    }
+
+    const text =
+      args.join(' ')
+
+    if (!text) {
+
+      return await sock.sendMessage(
+        m.key.remoteJid,
+        {
+          text:
+`╭━━〔 TESSIA GDESC 〕━━⬣
+┃ ⚠️ Donne une description
+╰━━━━━━━━━━━━⬣`
+        },
+        { quoted: m }
+      )
+    }
+
+    await sock.groupUpdateDescription(
+      m.key.remoteJid,
+      text
+    )
+
+    await sock.sendMessage(
+      m.key.remoteJid,
+      {
+        text:
+`╭━━〔 TESSIA GDESC 〕━━⬣
+
+┃ ✅ Description modifiée
+
+╰━━━━━━━━━━━━⬣`
+      },
+      { quoted: m }
+    )
+
+  } catch (e) {
+
+    console.log(e)
+  }
+}
