83e55e05f05fdac533a8dc25cb998385768f340d
Winter.Vfz <lisemdarenne@gmail.com>
2026-05-18 22:41:44 +0100

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -1,10 +1,74 @@
+// commands/owner.js
+
+import { config } from '../config.js'
+
 export const name = 'owner'
 
-export async function execute({ sock, m }) {
+export async function execute({
+  sock,
+  m,
+  thumb
+}) {
 
-  const owner = sock.user.id.split(':')[0]
+  try {
 
-  await sock.sendMessage(m.key.remoteJid, {
-    text: `👑 OWNER: ${owner}`
-  }, { quoted: m })
+    const owner =
+      config.owner
+
+    const text =
+`╭━━━〔 CRINSON BOT OWNER 〕━━━⬣
+
+┃ 👑 OWNER : ${owner}
+┃ 🤖 BOT : CRIMSON BOT UPD2
+┃ ⚡ STATUS : ONLINE
+
+┣━━〔 CHANNEL 〕━━⬣
+┃ 📢 Rejoins la chaîne
+┃ officielle CRIMSON :
+
+┃ ${config.channel}
+
+╰━━━━━━━━━━━━━━━━⬣`
+
+    await sock.sendMessage(
+      m.key.remoteJid,
+      {
+        image: thumb,
+
+        caption: text,
+
+        contextInfo: {
+
+          externalAdReply: {
+
+            title:
+              'CRIMSON OFFICIAL CHANNEL',
+
+            body:
+              'Clique pour rejoindre',
+
+            thumbnailUrl:
+'https://i.imgur.com/JPw4L1x.jpeg',
+
+            sourceUrl:
+              config.channel,
+
+            mediaType: 1,
+
+            renderLargerThumbnail: true,
+
+            showAdAttribution: true
+          }
+        }
+      },
+      { quoted: m }
+    )
+
+  } catch (e) {
+
+    console.log(
+      'OWNER ERROR:',
+      e
+    )
+  }
 }
